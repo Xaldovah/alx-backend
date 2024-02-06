96cf51449fe07db1513601cc48ec813952851b6a
xaldovah <dxaldovah@gmail.com>
2024-02-06 19:03:53 +0300

    Create a get_locale function with the babel.localeselector decorator. Use request.accept_languages to determine the best match with our supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""This module function creates a basic flask app"""
+
+import os
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+def get_locale():
+    """This function gets the locale of the client and returns
+    content with their preferred language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector=get_locale)
+
+
+class Config:
+    """This class provides the configuration of languages and timezone"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """Render the index.html template.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
